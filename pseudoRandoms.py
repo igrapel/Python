2fc8fe3d1f8802b5298310cedfeeb492c829b734
Ilan Grapel <igrapel@users.noreply.github.com>
2019-10-22 21:34:53 -0400

    Update pseudoRandoms.py

--- pseudoRandoms.py
+++ pseudoRandoms.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 18 07:57:59 2019
-
 @author: 323917
 """
 import numpy as np
@@ -11,38 +10,39 @@ import random
 
 """
 randomList=[]
-
 for x in range(12000):
     randInt=random.randint(1,10)
     randomList.append(randInt)
     
-
-
 counts = []
 for x in range(10):
     countDig = randomList.count(x)
     counts.append(countDig)
      
-
 objects = ('0','1', '2', '3', '4', '5', '6', '7', '8', '9')
 y_pos = np.arange(len(objects))
-
-
 plt.bar(y_pos, counts, align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
 plt.ylabel('Counts')
 plt.title('Digits')
-
 plt.show()
 """
-
+def getnumber():
+    while True:
+        number = input("Give me a digit: ")
+        if number.isdigit():
+            return int(number)
+        
+        
 n_groups = 10
 objects = ('0','1', '2', '3', '4', '5', '6', '7', '8', '9')
 
 randomList=[]
+studentList=[]
 
+num_Questions = int(input("How many digits to produce?" ))
 
-for x in range(1000):
+for x in range(num_Questions):
     randInt=random.randint(0,9)
     randomList.append(randInt)
     
@@ -52,12 +52,10 @@ for x in range(10):
     Random_counts.append(countDig)
 
 test = input("Ask the students? ")
-num_Questions = int(input("How many digits to produce?" ))
+
 
 if(test=="no"):
     
-    studentList=[]
-
     for x in range(num_Questions):
         randInt=random.randint(1,10)
         studentList.append(randInt)
@@ -70,7 +68,8 @@ if(test=="no"):
 
 else:
     for num in range(num_Questions):
-        studentNum= int(input("Give me a random number" ))
+        studentNum= getnumber()
+
         studentList.append(studentNum)
         
         Student_counts=[]
