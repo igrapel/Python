6416f8bbfdb4d856cc72e11d3f0ef7e0a86d7096
Ilan Grapel <igrapel@users.noreply.github.com>
2021-01-08 07:03:46 -0500

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,39 +1,23 @@
 import yfinance as yf
-import seaborn as sns
 
-
-#Open    High     Low   Close   Volume  Dividends  Stock Splits
-#time '2020-1-1'
-def stockHistory(symbol, s, e):
-    #define the ticker symbol
-    tickerSymbol = symbol
-
-    #get data on this ticker
-    tickerData = yf.Ticker(tickerSymbol)
-
-    #get the historical prices for this ticker
-    tickerDf = tickerData.history(period='1d', start= s, end= e)
-
-    return tickerDf    
-
-
-def stockHistoryColumn(symbol, s, e, col):
-    #define the ticker symbol
-    tickerSymbol = symbol
-
-    #get data on this ticker
-    tickerData = yf.Ticker(tickerSymbol)
-
-    #get the historical prices for this ticker
-    tickerDf = tickerData.history(period='1d', start= s, end= e)
+def stockHistorydf(stock, s, e):
+    tickerData = yf.Ticker(stock)
+    tickerDf = tickerData.history(period='id', start=s, end=e)
+    return tickerDf
     
-    tickerCol = tickerDf[col]
+def stockHistoryCol(stock, s, e, col):
+    myStockdf = stockHistorydf(stock, s, e)
+    myStockCol = myStockdf[col]
+    return myStockCol
+    
+mydata = stockHistoryCol('AAPL', '2020-3-1', '2020-12-12', 'Close')
 
-    return tickerCol    
+#Index(['Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'], dtype='object')
+#print(tickerDf['Close'])
 
-def graphHistory(symbol, s, e, col):
-    data = stockHistoryColumn(symbol, s, e, col)
-    data.plot(linewidth=0.5);
-
-print(stockHistoryColumn('HLT', '2020-6-1', '2021-1-1', 'Close'))
-graphHistory('PLTR', '2020-6-1', '2021-1-1', 'Close')
+def graphHistory(stock, s, e, col):
+    data = stockHistoryCol(stock, s, e, col)
+    data.plot(linewidth=0.5)
+    
+#graphHistory('AAPL', '2020-3-1', '2020-12-12', 'Close')
+print(type(mydata))
