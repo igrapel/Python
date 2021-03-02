bbd1f15d092e326e9ecf9a30dd2164a17baf6a26
Ilan Grapel <igrapel@users.noreply.github.com>
2021-03-01 23:01:03 -0500

    Update where.py

--- where.py
+++ where.py
@@ -1,10 +1,13 @@
 import re
 import json
 from urllib.request import urlopen
+import time
 
 url = 'http://ipinfo.io/json'
 response = urlopen(url)
 data = json.load(response)
+
+start = time.time()
 
 IP=data['ip']
 org=data['org']
@@ -14,3 +17,12 @@ region=data['region']
 
 print('Your IP detail\n ')
 print('IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP))
+
+
+key = ""
+while(key != "q"):
+    key = input("Thanks for visiting. Press q when you are done.")
+
+end = time.time()
+duration = end-start
+print("You were in our city for " + str(round(duration, 1)) + " seconds")
