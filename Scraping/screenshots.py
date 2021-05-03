ebeb763ae861bae0c7134627bb61db68de928cd0
Ilan Grapel <igrapel@users.noreply.github.com>
2021-05-03 10:35:36 -0400

    Update screenshots.py

--- Scraping/screenshots.py
+++ Scraping/screenshots.py
@@ -1,13 +1,15 @@
+
 #Imports Packages
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time 
 #Opens up web driver and goes to Google Images
-driver = webdriver.Chrome(executable_path=r'C:\Users\323917\OneDrive - Miami-Dade County Public Schools\Desktop\Portfolio\bin\chromedriver_win32\chromedriver.exe')
+driver = webdriver.Chrome(executable_path=r'C:\Users\grape\OneDrive\Desktop\Projects\Python\Flags\chromedriver.exe')
 driver.get('https://www.google.ca/imghp?hl=en&tab=ri&authuser=0&ogbl')
 
 box = driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input')
-box.send_keys('Cuba Flag')
+
+box.send_keys('Mexico Flag')
 box.send_keys(Keys.ENTER)
 
 #Will keep scrolling down the webpage until it cannot scroll no more
@@ -24,12 +26,15 @@ while True:
     if new_height == last_height:
         break
     last_height = new_height
+    
 
+address_base = 'C:\\Users\\grape\\OneDrive\\Desktop\\Projects\\Python\\Flags\\'
+address = address_base + 'MexicoFlag\\'
 
 for i in range(1, 200):
     try:
-        driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div['+str(i)+']/a[1]/div[1]/img').screenshot('C:\\Users\\323917\\OneDrive - Miami-Dade County Public Schools\\Desktop\\Portfolio\\CubaFlag\\flag('+str(i)+').png')
+        driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div['+str(i)+']/a[1]/div[1]/img').screenshot(address + 'flag('+str(i)+').png')
     except:
         pass
 
-print("jakisfj")
+print("Completed: ")
